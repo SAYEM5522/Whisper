9700b785cd6178fe06740a7949ed12d5a6ebe85e
SAYEM5522 <md1040582@gmail.com>
2022-12-01 18:30:46 +0600

    Dark Mode Add In Header

--- whisper_web/components/Header/Header.tsx
+++ whisper_web/components/Header/Header.tsx
@@ -1,4 +1,8 @@
 import React,{useState,useCallback} from 'react'
+import {BsSun} from "react-icons/bs"
+import {HiOutlineMoon} from "react-icons/hi"
+import { useTheme } from 'next-themes'
+
 const HeaderItem=[
   {
     id:1,
@@ -15,10 +19,26 @@ const HeaderItem=[
 ]
 
 const Header = () => {
+  const [mode,setMode]=useState(false)
+  const { theme, setTheme } = useTheme()
   const [acttiveIndex,setACtiveIndex]=useState(0)
+  const ModeChange=useCallback(()=>{
+    setMode(!mode)
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  },[mode,theme])
   return (
     <div>
+      <div className='flex items-center'>
       <h2 className='font-semibold italic text-3xl py-3'>Whisper</h2>
+      <div onClick={ModeChange}>
+      {
+        mode?
+        <HiOutlineMoon size={25} className="ml-5 mt-1 cursor-pointer"/>:
+        <BsSun  size={25} className="ml-5 mt-1 cursor-pointer"/>
+
+      }
+      </div>
+      </div>
     <div 
     className='max-w-full flex items-center'
     >
@@ -27,7 +47,7 @@ const Header = () => {
       HeaderItem.map((item,index)=>{
         return(
           <div key={index} className={`${index===acttiveIndex ?'border-x border-t rounded-[3px]':'border-b hover:bg-[#EEEEEE] hover:rounded-sm '} cursor-pointer gray-400 px-4 py-3`}>
-            <p onClick={()=>setACtiveIndex(index)} className={`${acttiveIndex===index?'text-black':'text-[#A43333]'} font-semibold `}>{item.name}</p>
+            <p onClick={()=>setACtiveIndex(index)} className={`${acttiveIndex===index?'text-black dark:text-white':'text-[#A43333]'} font-semibold `}>{item.name}</p>
           </div>
         )
       })
