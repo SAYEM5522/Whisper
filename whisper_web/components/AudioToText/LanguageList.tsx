cc152079ca8247aa547d84c0a252a1d8e73fdc02
SAYEM5522 <md1040582@gmail.com>
2022-12-15 23:42:26 +0600

    Language List update

--- whisper_web/components/AudioToText/LanguageList.tsx
+++ whisper_web/components/AudioToText/LanguageList.tsx
@@ -4,12 +4,12 @@ const LanguageList = () => {
   return (
     <div>
       <p className='font-medium text-md'>Supported Language</p>
-    <div className='h-[29rem] w-60 p-2  overflow-y-auto [&::-webkit-scrollbar]:hidden'>
+    <div className='h-[29rem] w-52 p-2  overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:hidden'>
       {
         LANGUAGES.map((item,index:number)=>{
           return(
-            <div key={index}>
-             <p>{Object.values(item)}</p>
+            <div key={index} className='w-52 bg-gray-50 m-1 p-[1.5px] rounded-md text-center cursor-pointer text-[#A07855FF] italic font-serif' >
+             <p className='first-letter: capitalize'>{item.language}</p>
             </div>
           )
         })
